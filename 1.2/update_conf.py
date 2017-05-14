42e4304000fe7f934295670d0180bc4ac312e4e1
blacktop <blacktop@users.noreply.github.com>
2017-05-14 15:38:04 -0600

    update dockerfile

--- 1.2/update_conf.py
+++ 1.2/update_conf.py
@@ -4,20 +4,20 @@ import os
 import sys
 import ConfigParser
 
-cfg = ConfigParser.ConfigParser()
-cfg.read("/cuckoo/conf/reporting.conf")
+reporting_cfg = ConfigParser.ConfigParser()
+cuckoo_cfg = ConfigParser.ConfigParser()
 
+reporting_cfg.read("/cuckoo/conf/reporting.conf")
 with open("/cuckoo/conf/reporting.conf", 'w') as cfile:
     if os.environ.get('MONGO_HOST'):
-        cfg.set('mongodb', 'enabled', True)
-        cfg.set('mongodb', 'hosts', os.environ['MONGO_HOST'])
-    cfg.write(cfile)
+        reporting_cfg.set('mongodb', 'enabled', True)
+        reporting_cfg.set('mongodb', 'hosts', os.environ['MONGO_HOST'])
+    reporting_cfg.write(cfile)
 
-cfg.read("/cuckoo/conf/cuckoo.conf")
-
+cuckoo_cfg.read("/cuckoo/conf/cuckoo.conf")
 with open("/cuckoo/conf/cuckoo.conf", 'w') as cfile:
     if os.environ.get('RESULTSERVER'):
-        cfg.set('resultserver', 'ip', os.environ['RESULTSERVER'])
-    cfg.write(cfile)
+        cuckoo_cfg.set('resultserver', 'ip', os.environ['RESULTSERVER'])
+    cuckoo_cfg.write(cfile)
 
 sys.exit()

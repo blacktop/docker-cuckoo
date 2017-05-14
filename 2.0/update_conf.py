f773daf433715a9f991efcfc0c3494b0de12de6b
blacktop <blacktop@users.noreply.github.com>
2017-05-14 15:05:19 -0600

    fix possible issue with using the same ConfigParser object

--- 2.0/update_conf.py
+++ 2.0/update_conf.py
@@ -4,23 +4,23 @@ import os
 import sys
 import ConfigParser
 
-cfg = ConfigParser.ConfigParser()
-cfg.read("/cuckoo/conf/reporting.conf")
+reporting_cfg = ConfigParser.ConfigParser()
+cuckoo_cfg = ConfigParser.ConfigParser()
 
+reporting_cfg.read("/cuckoo/conf/reporting.conf")
 with open("/cuckoo/conf/reporting.conf", 'w') as cfile:
     if os.environ.get('ES_HOST'):
-        cfg.set('elasticsearch', 'enabled', "yes")
-        cfg.set('elasticsearch', 'hosts', os.environ['ES_HOST'])
+        reporting_cfg.set('elasticsearch', 'enabled', "yes")
+        reporting_cfg.set('elasticsearch', 'hosts', os.environ['ES_HOST'])
     if os.environ.get('MONGO_HOST'):
-        cfg.set('mongodb', 'enabled', "yes")
-        cfg.set('mongodb', 'hosts', os.environ['MONGO_HOST'])
-    cfg.write(cfile)
+        reporting_cfg.set('mongodb', 'enabled', "yes")
+        reporting_cfg.set('mongodb', 'hosts', os.environ['MONGO_HOST'])
+    reporting_cfg.write(cfile)
 
-cfg.read("/cuckoo/conf/cuckoo.conf")
-
+cuckoo_cfg.read("/cuckoo/conf/cuckoo.conf")
 with open("/cuckoo/conf/cuckoo.conf", 'w') as cfile:
     if os.environ.get('RESULTSERVER'):
-        cfg.set('resultserver', 'ip', os.environ['RESULTSERVER'])
-    cfg.write(cfile)
+        cuckoo_cfg.set('resultserver', 'ip', os.environ['RESULTSERVER'])
+    cuckoo_cfg.write(cfile)
 
 sys.exit()
